c313bb01a9cdc76ceace8212566e547fee952c25
wi11iamFernandez <william.fernandez@edu.itspiemonte.it>
2024-10-15 17:44:25 +0200

    login

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login-form',
@@ -18,21 +19,34 @@ import { MatIconModule } from '@angular/material/icon';
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss'
 })
+
+@Injectable({ providedIn: 'root' })
 export class LoginFormComponent {
   loginForm: FormGroup;
   hide = true;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private http: HttpClient) {
     this.loginForm = this.fb.group({
-      username: ['', Validators.required],
+      email: ['', Validators.required],
       password: ['', Validators.required]
     });
   }
 
   onSubmit() {
     if (this.loginForm.valid) {
-      const { username, password } = this.loginForm.value;
-      console.log('Username:', username, 'Password:', password);
+      const { email, password } = this.loginForm.value;
+      this.http.post('http://localhost:9006/api/auth/authenticate', { email, password })
+        .subscribe({
+          next: (response) => {
+            // Gestisci la risposta, ad esempio, memorizza il token o reindirizza
+            console.log('Login successful:', response);
+            
+          },
+          error: (error) => {
+            console.error('Login failed:', error);
+            
+          }
+        });
     }
   }
 
